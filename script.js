da1fb981da2a08a2ef2179798cc6c16e885bf10d
Arti2911 <139011882+Arti2911@users.noreply.github.com>
2024-11-28 00:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function convertToCapitalCase() {
+	const elements = document.getElementById("fname").focus();
+	 elements.forEach(element => {
+		 const capitalizedText = elements
+      .toLowerCase()
+      .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+
+    element.textContent = capitalizedText;
+	 })
+}
